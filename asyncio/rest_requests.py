99db3691f4b482c1bebbb6ac77f8552ce4b9f652
norkos <norbert.kosmal@gmail.com>
2022-10-14 09:02:07 +0200

    Learning iohttp to GET the data

--- asyncio/rest_requests.py
+++ asyncio/rest_requests.py
@@ -1,6 +1,11 @@
 import requests
 import time
 import asyncio
+import aiohttp
+
+import platform
+if platform.system() == 'Windows':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 
 def getting_pokemons_sync_way(how_many: int) -> None:
@@ -10,29 +15,21 @@ def getting_pokemons_sync_way(how_many: int) -> None:
         url = f'https://pokeapi.co/api/v2/pokemon/{number}'
         response = requests.get(url)
         name = response.json()['name']
-#        print(name)
+        #print(name)
     print('It took me %0.2f ms' % (1000 * (time.time() - t0)))
-
-
-async def get_pokemon(id: int) -> None:
-    url = f'https://pokeapi.co/api/v2/pokemon/{id}'
-    response = await requests.get(url)
-    name = response.json()['name']
 
 
 async def getting_pokemons_async_way(how_many: int) -> None:
     print('Getting pokemons in async way')
     t0 = time.time()
-    tasks = []
-    for number in range(1, how_many):
-        tasks.append(asyncio.create_task(get_pokemon(number)))
-
-    await asyncio.wait(tasks)
+    async with aiohttp.ClientSession() as session:
+        for number in range(1, how_many):
+            async with session.get(f'https://pokeapi.co/api/v2/pokemon/{number}') as response:
+                result = await response.json()
+                name = result['name']
+                #print(name)
     print('It took me %0.2f ms' % (1000 * (time.time() - t0)))
 
 if __name__ == "__main__":
     getting_pokemons_sync_way(50)
-
-    loop = asyncio.new_event_loop()
-    loop.run_until_complete(getting_pokemons_async_way(50))
-    loop.close()
+    asyncio.run(getting_pokemons_async_way(50))
