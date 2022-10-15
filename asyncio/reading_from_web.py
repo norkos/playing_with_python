c83b3b422881fd1b9dc7101a0c619a7342fc79b8
norkos <norbert.kosmal@gmail.com>
2022-10-15 15:31:32 +0200

    Reading web-page in async mode

--- asyncio/reading_from_web.py
+++ asyncio/reading_from_web.py
@@ -1,3 +1,5 @@
+import asyncio
+import platform
 import httpx
 import time
 import pprint
@@ -5,6 +7,9 @@ import pprint
 pp = pprint.PrettyPrinter(indent=2)
 
 links = ['recipes', 'easyrecipes', 'TopSecretRecipes']
+
+if platform.system() == 'Windows':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
 
 def get_the_content(url: str) -> list[tuple[str, int, str]]:
@@ -17,7 +22,7 @@ def get_the_content(url: str) -> list[tuple[str, int, str]]:
     return results
 
 
-def get_the_data_in_sync_way() -> None:
+def get_the_data() -> None:
     print('Getting data in sync way')
     t0 = time.time()
     result = {}
@@ -27,5 +32,32 @@ def get_the_data_in_sync_way() -> None:
     pp.pprint(result)
 
 
+async def get_the_content_in_async(session: httpx.AsyncClient, url: str) -> list[tuple[str, int, str]]:
+    r = await session.get(url)
+    results = []
+    for record in r.json()['data']['children']:
+        title = record['data']['title']
+        score = record['data']['score']
+        results.append((title, score))
+    return results
+
+
+async def get_the_data_in_async() -> None:
+    print('Getting data in async way')
+    t0 = time.time()
+    tasks = []
+    async with httpx.AsyncClient() as session:
+        for link in links:
+            tasks.append(
+                asyncio.create_task(
+                    get_the_content_in_async(session,
+                    f'https://www.reddit.com/r/{link}/top.json?sort=top&t=day&limit=5')
+                )
+            )
+        results = await asyncio.gather(*tasks)
+    print('It took %0.2f ms' % (1000 * (time.time() - t0)))
+    pp.pprint(results)
+
 if __name__ == "__main__":
-    get_the_data_in_sync_way()
+    get_the_data()
+    asyncio.run(get_the_data_in_async())
